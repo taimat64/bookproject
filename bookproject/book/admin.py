3fbde8c156b203cff4f64187c3405c18856bead5
taimat64 <106381601+taimat64@users.noreply.github.com>
2024-06-26 18:13:03 +0900

    Add files via upload

---
+++ bookproject/book/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+#from .models import SampleModel
+from .models import Book, Review
+
+#admin.site.register(SampleModel)
+admin.site.register(Book)
+admin.site.register(Review)
